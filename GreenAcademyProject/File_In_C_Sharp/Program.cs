d4d5e04a8335735d96c1a9c96e507104d4c7a80f
pnu <pnu2410@gmail.com>
2024-04-11 20:24:28 +0700

    heheboiz_mlem_mlem

--- GreenAcademyProject/File_In_C_Sharp/Program.cs
+++ GreenAcademyProject/File_In_C_Sharp/Program.cs
@@ -1,34 +1,45 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text.Json;
 
 namespace File_In_C_Sharp
 {
-    public class SinhVien
+    public class Character
     {
-        public string Full_Name { get; set; }
-        public int Age { get; set; }
-        public string Email { get; set; }
-        public SinhVien() { }
+        public string Name { get; set; }
+        public string Class { get; set; }
+        public string Hp { get; set; }
+        public string Damage { get; set; }
+        public Character() { }
+    }
+    public class RootObject
+    {
+        [JsonProperty("Players")]
+        public List<Character> Players { get; set; }
+        [JsonProperty("Enemies")]
+        public List<Character> Enemies { get; set; }
     }
     internal class Program
     {
 
         static void Main(string[] args)
         {
-            string file_Path = "C:/Users/MAY01/Downloads/GreenAcademy/GreenAcademyProject/File_In_C_Sharp/Test.json";
-            //string text = "Hello World";
-            //File.WriteAllText(file_Path, text);
-            //Console.WriteLine(File.ReadAllText(file_Path));
-            var opt = new JsonSerializerOptions()
-            { WriteIndented = true };
-            string read_File = File.ReadAllText(file_Path);
-            SinhVien sv = JsonSerializer.Deserialize<SinhVien>(read_File);
-            Console.WriteLine("Full Name: " + sv.Full_Name);
-            Console.WriteLine("Age: " + sv.Age);
-            Console.WriteLine("Email: " + sv.Email);
-            
+            string file_Path = "C:/Users/May1/Downloads/GreenAcademy/GreenAcademyProject/File_In_C_Sharp/Test.json";
+
+            string  read_Json = File.ReadAllText(file_Path);
+
+            RootObject root = JsonConvert.DeserializeObject<RootObject>(read_Json);
+
+            foreach (var player in root.Players)
+            {
+                Console.WriteLine($"Name: {player.Name}, HP: {player.Hp}, Class: {player.Class}, Damage: {player.Damage}");
+            }
+            foreach (var enemy in root.Enemies)
+            {
+                Console.WriteLine($"Name: {enemy.Name}, HP: {enemy.Hp}, Class: {enemy.Class}, Damage: {enemy.Damage}");
+            }
         }
     }
 }
